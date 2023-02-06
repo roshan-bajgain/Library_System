9b24a9a81dd42e0cf7fb971754d4efe67e2a2541
roshan-bajgain <roshan.bazgain@gmail.com>
2023-02-06 20:31:25 +0545

    Add add functionality

--- Library.c
+++ Library.c
@@ -1,37 +1,107 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-//This is the book structure
-void addstudent();
-void studentrecord();
-void searchstudent();
-void delete();
+struct books
+{
+    int id;
+    char bookName[50];
+    char authorName[50];
+    char date[12];
+}b;
 
 struct student
 {
-    /* data */
-    char Name[20];
-    int roll_no;
-    int class[10];
-    char vill[20];
-    float per;
-};
+    int id; 
+    char sName[50];
+    char sClass[50];
+    int sRoll;
+    char bookName[50];
+    char date[12];
+}s;
 
-void main() {
-    int choice;
-    while (choice != 5)
+FILE *fp;
+
+int main () {
+    int ch;
+    while (1)
     {
-        printf("\t\t\t ==== STUDENT RECORD====");
-        printf("\n\n\n\t\t\t\t 1. Add Student Record\n");
-        printf("\t\t\t\t 2. Student Record\n");
-        printf("\t\t\t\t 3. Search Student\n");
-        printf("\t\t\t\t 4. Delete Record\n");
-        printf("\t\t\t\t 5. Exist\n");
-        printf("\t\t\t\t_____________________________________\n");
-        printf("\t\t\t\t  ");
-        scanf("%d", &choice);
+        /* code */
+        system("cls");
+        printf("<=== Library Management System ===>");
+        printf("1. Add Book \n");
+        printf("2. Books list \n");
+        printf("3. Remove Book \n");
+        printf("4. Issued Book \n");
+        printf("5. List Issued book \n");
+        printf("0. Exit \n\n");
+        printf("Enter your choice");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 0;
+            /* code */
+            exit(0);
+        case 1: 
+          addBook();
+          break;
+
+        case 2:
+          bookslist();
+          break;
+
+        case 3:
+          del();
+          break;  
+
+        case 4;
+          issueBook();
+          break;
+
+        case 5;
+          issueList();
+          break;
+        
+        default:
+          printf("Invalid choice... \n\n");
+        }
+          printf("Predd any key to continue...");
     }
-    
+    return 0;
 }
+
+void addBook() {
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate,"%02d/%02d/%02d",tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(b.date, myDate);
+
+    fp = fopen("books.txt","ab");
+
+    printf("Enter book id: ");
+    scanf("%d", &b.id);
+
+    printf("Enter book name: ");
+    fflush(stdin);
+    gets(b.bookName);
+
+    printf("Enter author name: ");
+    fflush(stdin);
+    gets(b.authorName);
+
+    printf("Book added Successfully");
+
+    fwrite(&b, sizeof(b),1,fp);
+    fclose(fp);
+}
+
+
+
+
+
+
 
 
 
