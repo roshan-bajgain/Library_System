d56a4bb38137cdb31b8f3d8bffb06ebf8644d350
roshan-bajgain <roshan.bazgain@gmail.com>
2023-02-19 20:15:25 +0545

    Fix error

--- Library.c
+++ Library.c
@@ -50,14 +50,14 @@ void addBook() {
     fclose(fp);
 }
 
-void bookList() {
+void booksList() {
     system("cls");
-    printf("<== Avaialable Book ==>");
-    printf("%-10s %-30s %20s %s", "Book Id","Book Name", "Author","Date");
+    printf("<== Avaialable Book ==>\n");
+    printf("%-10s %-30s %20s %s\n\n", "Book id","Book Name", "Author","Date");
     fp = fopen("books.txt","rb");
     while (fread(&b,sizeof(b),1,fp)== 1)
     {
-        printf("%-10d %-30s %-20s %s\n\n",b.id,b.bookName,b.authorName,b.date);
+        printf("%-10d %-30s %-20s %s\n\n",b.id, b.bookName, b.authorName, b.date);
     }
     fclose(fp);
     
@@ -87,6 +87,11 @@ int main () {
           case 1: 
             addBook();
             break;
+
+           case 2:
+           booksList();
+           break;
+
 
           default:
             printf("Invalid choice... \n\n");
