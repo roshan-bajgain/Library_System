4ab3dfd113c81fa9187eb1c93eda7e030f1ec65f
roshan-bajgain <roshan.bazgain@gmail.com>
2023-02-26 12:48:47 +0545

    Add issue functionality

--- Library.c
+++ Library.c
@@ -67,7 +67,7 @@ void del() {
     int id, f=0;
     system("cls");
     printf("<== Remove Books ==>\n\n");
-    printf("Enter book id to remove");
+    printf("Enter book id to remove: ");
     scanf("%d", id);
     FILE *ft;
     fp= fopen("books.txt","rb");
@@ -89,6 +89,54 @@ void del() {
     fclose(ft);
     remove("books.txt");
     rename("temp.txt","books.txt");
+}
+
+void issueBook () {
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate,"%02d/%02d/%02d",tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(s.date, myDate);
+
+    int f = 0;
+    system("cls");
+    printf("<== Issue Books ==>\n\n");
+    printf("Enter book id to issue: ");
+    scanf("%d", &s.id);
+
+    fp = fopen("books.txt","rb");
+    while (fread(&b,sizeof(b),1,fp) == 1)
+    {
+        if (b.id == s.id) {
+            strcpy(s.bookName, b.bookName);
+            f=1;
+            break;
+        }
+    }
+    if (f==0) {
+        printf("No book found with this id \n");
+        printf("Please try again... \n\n");
+        return;
+    }
+    fp = fopen("issue.txt","rb");
+
+    printf("Enter Student Name: ");
+    fflush(stdin);
+    gets(s.sName);
+
+    printf("Enter Student class: ");
+    fflush(stdin);
+    gets(s.sClass);
+
+    printf("Enter Student Roll no: ");
+    fflush(stdin);
+    gets(s.sRoll);
+
+    printf("Book issue Successfully\n\n");
+
+    fwrite(&s, sizeof(s),1,fp); //we write on issue.txt all the details
+    fclose(fp);
+    
 }
 
 int main () {
@@ -117,8 +165,16 @@ int main () {
             break;
 
            case 2:
-           booksList();
-           break;
+             booksList();
+             break;
+
+           case 3:
+             del();
+             break;
+
+           case 4:
+             issueBook();
+             break;
 
 
           default:
