55b21b15599663f2fd2e8923cbfbb289092e48b2
roshan-bajgain <roshan.bazgain@gmail.com>
2023-02-26 15:38:38 +0545

    Add Issue book funtionality

--- Library.c
+++ Library.c
@@ -17,7 +17,6 @@ struct student
     int id; 
     char sName[50];
     char sClass[50];
-    int sRoll;
     char bookName[50];
     char date[12];
 }s;
@@ -44,7 +43,7 @@ void addBook() {
     fflush(stdin);
     gets(b.authorName);
 
-    printf("Book added Successfully");
+    printf("Book added Successfully\n\n");
 
     fwrite(&b, sizeof(b),1,fp);
     fclose(fp);
@@ -52,8 +51,8 @@ void addBook() {
 
 void booksList() {
     system("cls");
-    printf("\t\t\t\t\t\t\t\t<== Avaialable Book ==>\n\n");
-    printf("%-10s %-30s %s\t\t\t\t\t\t %s\t\t\t\n", "Book id","Book Name", "Author", "Date");
+    printf("\t\t\t\t\t<== Avaialable Book ==>\n\n");
+    printf("%-10s %-30s %-20s %s\n", "Book id","Book Name", "Author", "Date");
     fp = fopen("books.txt","rb");
     while (fread(&b,sizeof(b),1,fp)== 1)
     {
@@ -68,8 +67,10 @@ void del() {
     system("cls");
     printf("<== Remove Books ==>\n\n");
     printf("Enter book id to remove: ");
-    scanf("%d", id);
+    scanf("%d", &id);
+
     FILE *ft;
+
     fp= fopen("books.txt","rb");
     ft = fopen("temp.txt","wb");
     
@@ -81,7 +82,7 @@ void del() {
         }
     }
     if(f==1) {
-        printf("\n\n Delete Successfully");
+        printf("\n\n Delete Successfully\n");
     } else {
         printf("\n\n Record not found");
     }
@@ -98,13 +99,14 @@ void issueBook () {
     sprintf(myDate,"%02d/%02d/%02d",tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
     strcpy(s.date, myDate);
 
-    int f = 0;
+    int f=0;
     system("cls");
     printf("<== Issue Books ==>\n\n");
     printf("Enter book id to issue: ");
     scanf("%d", &s.id);
 
     fp = fopen("books.txt","rb");
+
     while (fread(&b,sizeof(b),1,fp) == 1)
     {
         if (b.id == s.id) {
@@ -115,10 +117,11 @@ void issueBook () {
     }
     if (f==0) {
         printf("No book found with this id \n");
-        printf("Please try again... \n\n");
+        printf("Please try again...\n\n");
         return;
     }
-    fp = fopen("issue.txt","rb");
+
+    fp = fopen("issue.txt","ab"); //ab= append in binary mode
 
     printf("Enter Student Name: ");
     fflush(stdin);
@@ -128,13 +131,21 @@ void issueBook () {
     fflush(stdin);
     gets(s.sClass);
 
-    printf("Enter Student Roll no: ");
-    fflush(stdin);
-    gets(s.sRoll);
-
     printf("Book issue Successfully\n\n");
 
     fwrite(&s, sizeof(s),1,fp); //we write on issue.txt all the details
+    fclose(fp);
+}
+
+void issueList() {
+    system("cls");
+    printf("\t\t\t\t\t<== Book Issue List ==>\n\n");
+    printf("%-10s %-30s %-20s %s\n\n", "Id","Name", "Class", "Book Name", "Date");
+    fp = fopen("issue.txt","rb");
+    while (fread(&s,sizeof(s),1,fp)== 1)
+    {
+        printf("%-10d %-30s %-20s %s\n\n",s.id, s.sName, s.sClass, s.bookName,s.date);
+    }
     fclose(fp);
     
 }
@@ -175,6 +186,10 @@ int main () {
            case 4:
              issueBook();
              break;
+
+           case 5:
+             issueList();
+             break;  
 
 
           default:
